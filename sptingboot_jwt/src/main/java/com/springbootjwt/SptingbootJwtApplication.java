2a1857f79aeec76929b67a732142487cb2e6355b
wxy <1483863489@qq.com>
2017-08-28 17:13:20 +0800

    添加springboot-jwt

---
+++ sptingboot_jwt/src/main/java/com/springbootjwt/SptingbootJwtApplication.java
@@ -0,0 +1,12 @@
+package com.springbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptingbootJwtApplication.class, args);
+	}
+}
