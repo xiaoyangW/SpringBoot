b4f487db9d073664ee48cf2fd38aa8060f4e139b
xiaoyang <1483863489@qq.com>
2018-04-10 11:48:11 +0800

    更新plugin

--- springboot-docker/src/main/java/com/springbootdocker/SpringbootDockerApplication.java
+++ springboot-docker/src/main/java/com/springbootdocker/SpringbootDockerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author WXY
+ */
 @SpringBootApplication
 @RestController
 public class SpringbootDockerApplication {
