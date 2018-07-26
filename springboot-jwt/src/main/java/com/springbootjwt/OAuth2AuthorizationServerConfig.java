16a3ec13a95d5d86c7a28c93e5b9fd5c3aac5c5b
xiaoyang <1483863489@qq.com>
2018-07-26 14:49:35 +0800

    oauth

---
+++ springboot-jwt/src/main/java/com/springbootjwt/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,11 @@
+package com.springbootjwt;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author WXY
+ * @date 2018/7/26 14:39
+ */
+@Configuration
+public class OAuth2AuthorizationServerConfig {
+}
