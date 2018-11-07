64fdfedf4b16dfa7225beacf5b1e0c0265e73dd8
xiaoyang <1483863489@qq.com>
2018-11-07 20:10:13 +0800

    redis data type test

--- springboot-redis/src/test/java/com/springbootredis/RedisApplicationTests.java
+++ springboot-redis/src/test/java/com/springbootredis/RedisApplicationTests.java
@@ -4,13 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,15 +17,42 @@ public class RedisApplicationTests {
 	RedisTemplate<String,String> redisTemplate;
 
 	@Test
-	public void contextLoads() {
-		/*ValueOperations<String, String> valueOperations= redisTemplate.opsForValue();
-		valueOperations.set("name","晓阳");*/
+	public void redisString() {
+		//redis string
+		ValueOperations<String, String> valueOperations= redisTemplate.opsForValue();
+		valueOperations.set("name","晓阳");
+
+	}
+	@Test
+	public void redisHash(){
+		//redis hash
 		HashOperations<String, String, String> hashOperations= redisTemplate.opsForHash();
 		Map<String,String> map = new HashMap<>(3);
 		map.put("name2","name22");
 		map.put("name3","name2233");
 		map.put("name4","name2244");
 		hashOperations.putAll("names",map);
+	}
+
+	@Test
+	public void redisSet(){
+        SetOperations<String, String> opsForSet = redisTemplate.opsForSet();
+        //System.out.println(opsForSet.add("set_test","1","2","3"));
+        System.out.println(opsForSet.members("set_test"));
+	}
+    @Test
+	public void redisZSet(){
+        ZSetOperations<String, String> opsForZSet = redisTemplate.opsForZSet();
+        Set<ZSetOperations.TypedTuple<String>> set = new HashSet<>();
+        set.add(new DefaultTypedTuple<>("w", (double) 1L));
+        set.add(new DefaultTypedTuple<>("xy", (double) 2L));
+        set.add(new DefaultTypedTuple<>("t", (double) 3L));
+        opsForZSet.add("zset_test",set);
+    }
+    @Test
+    public void redisList(){
+        ListOperations<String, String> opsForList = redisTemplate.opsForList();
+        opsForList.leftPushAll("list",Arrays.asList("1","2","3"));
 	}
 
 }
